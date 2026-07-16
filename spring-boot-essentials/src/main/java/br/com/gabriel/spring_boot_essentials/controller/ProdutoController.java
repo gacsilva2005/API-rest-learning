cdc4661f5ebc68f38aa2da30a777bac670107de1
Gabriel Silva <donnerff076@gmail.com>
2026-07-16 16:31:33 -0300

    fix(product): change GET method to return a list of products

--- spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/controller/ProdutoController.java
+++ spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/controller/ProdutoController.java
@@ -1,5 +1,6 @@
 package br.com.gabriel.spring_boot_essentials.controller;
 
+import br.com.gabriel.spring_boot_essentials.model.ProdutoEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,17 +10,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.gabriel.spring_boot_essentials.service.ProdutoService;
 
+import java.util.List;
+
 //beens do Spring Boot
 @RestController
 @RequestMapping("/v1/produtos")
 @RequiredArgsConstructor
 public class ProdutoController {
 
+    //Injetando a classe que vai fornecer os métodos
     private final ProdutoService produtoService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String findAll(){
-        return "Lista de produtos";
+    public List<ProdutoEntity> findAll(){
+        return produtoService.findAll();
     }
 }
