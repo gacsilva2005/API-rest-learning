52a4e8815d4ac12c726531031e0360d8866d1dca
Gabriel Silva <donnerff076@gmail.com>
2026-07-16 15:52:10 -0300

    fix(structure): move project source out of .idea and untrack IDE files

---
+++ spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/controller/ProdutoController.java
@@ -0,0 +1,25 @@
+package br.com.gabriel.spring_boot_essentials.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.gabriel.spring_boot_essentials.service.ProdutoService;
+
+//beens do Spring Boot
+@RestController
+@RequestMapping("/v1/produtos")
+@RequiredArgsConstructor
+public class ProdutoController {
+
+    private final ProdutoService produtoService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String findAll(){
+        return "Lista de produtos";
+    }
+}
