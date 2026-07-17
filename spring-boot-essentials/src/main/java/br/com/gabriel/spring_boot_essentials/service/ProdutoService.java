5376664bf09d4d81f32f0d635bad07217fc566d3
Gabriel Silva <donnerff076@gmail.com>
2026-07-17 15:54:04 -0300

    feat(produto): add updateProduto method to update a product by ID in the list

--- spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/service/ProdutoService.java
+++ spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/service/ProdutoService.java
@@ -62,4 +62,20 @@ public class ProdutoService {
 
         return novoProduto;
     }
+
+    public ProdutoEntity updateProduto(ProdutoDTO produtoDTO, Integer id){
+
+        //Procura o produto filtrando pelo ID
+        ProdutoEntity produto = PRODUTOS.stream()
+                .filter(p -> p.getId().equals(id))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+
+        //Atualiza os fields do produto encotrado pelo ID
+        produto.setNome(produto.getNome());
+        produto.setPreco(produto.getPreco());
+        produto.setQuantidade(produto.getQuantidade());
+
+        return produto;
+    }
 }
