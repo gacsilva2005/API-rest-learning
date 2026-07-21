5c1aec5df5719d82c717bd91192c265bfde1e020
Gabriel Silva <donnerff076@gmail.com>
2026-07-21 16:17:42 -0300

    feat(carro): add new method to create a new object

--- spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/service/CarroService.java
+++ spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/service/CarroService.java
@@ -1,5 +1,7 @@
 package br.com.gabriel.spring_boot_essentials.service;
 
+import br.com.gabriel.spring_boot_essentials.dto.CarroDTO;
+import br.com.gabriel.spring_boot_essentials.dto.ProdutoDTO;
 import br.com.gabriel.spring_boot_essentials.model.CarroEntity;
 import br.com.gabriel.spring_boot_essentials.model.ProdutoEntity;
 import org.springframework.stereotype.Service;
@@ -41,5 +43,30 @@ public class CarroService {
     //Método para listar todos os produtos
     public List<CarroEntity> findAll() {
         return new ArrayList<>(CARROS);
+    }
+
+    //Método para criação de novo objeto, passando o DTO como parâmetro para obter os dados
+    public CarroEntity createCarro(CarroDTO carroDTO){
+
+        //Função para incrementar o id na criação do produto
+        Integer identificador = CARROS.stream() //transforma a lista em stream pra filtrar
+                .mapToInt(CarroEntity::getId) //cada objeto vira seu respectivo ID
+                .max() //acha o maior ID
+                .orElse(0) + 1; /** adiciona 1 no maior ID para gerar o próximo se
+                                        existir algum objeto, se não, o máximo é 0**/
+
+        //Passa os fields do DTO e cria uma nova entidade
+        CarroEntity novoCarro = CarroEntity.builder()
+                .id(identificador) //passa o id como parâmetro pra construção do objeto
+                .nome(carroDTO.getNome()) //GET nome do DTO
+                .preco(carroDTO.getPreco()) //GET preco do DTO
+                .potencia(carroDTO.getPotencia()) //GET potencia do DTO
+                .build(); //builda nova entidade
+
+        //Adiciona novo carro na lista mockada de dados
+        CARROS.add(novoCarro);
+
+        //retorna o novo carro criado
+        return novoCarro;
     }
 }
