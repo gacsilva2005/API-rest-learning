3cfc969d727c4bf196a32266f802e4c57275c837
Gabriel Silva <donnerff076@gmail.com>
2026-07-21 16:17:01 -0300

    feat(carro): add HTTP POST method

--- spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/controller/CarroController.java
+++ spring-boot-essentials/src/main/java/br/com/gabriel/spring_boot_essentials/controller/CarroController.java
@@ -1,15 +1,14 @@
 package br.com.gabriel.spring_boot_essentials.controller;
 
+import br.com.gabriel.spring_boot_essentials.dto.CarroDTO;
+import br.com.gabriel.spring_boot_essentials.dto.ProdutoDTO;
 import br.com.gabriel.spring_boot_essentials.model.CarroEntity;
 import br.com.gabriel.spring_boot_essentials.model.ProdutoEntity;
 import br.com.gabriel.spring_boot_essentials.service.CarroService;
 import br.com.gabriel.spring_boot_essentials.service.ProdutoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +26,12 @@ public class CarroController {
     @ResponseStatus(HttpStatus.OK)
     public List<CarroEntity> findAll(){
         return carroService.findAll();
+    }
+
+    //Método Post para adicional novo carro na lista
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CarroEntity createCarro(@RequestBody CarroDTO carroDTO) {
+        return carroService.createCarro(carroDTO);
     }
 }
